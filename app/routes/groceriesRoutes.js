6f04dc7685516417b8115bf8dc8d09e627beadcd
Maxwell Kwarteng <maxtheflash007@gmail.com>
2020-09-23 23:12:05 -0400

    Added modified groceriesRoutes.js to API

--- app/routes/groceriesRoutes.js
+++ app/routes/groceriesRoutes.js
@@ -1,26 +1,41 @@
+// Express docs: http://expressjs.com/en/api.html
 const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
+
+// pull in Mongoose model for examples
+const Groceries = require('../models/groceries')
+
+// this is a collection of methods that help us detect situations when we need
+// to throw a custom error
+const customErrors = require('../../lib/custom_errors')
+
+// we'll use this function to send 404 when non-existant document is requested
+const handle404 = customErrors.handle404
+// we'll use this function to send 401 when a user tries to modify a resource
+// that's owned by someone else
+// const requireOwnership = customErrors.requireOwnership
+
+// this is middleware that will remove blank fields from `req.body`, e.g.
+// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
+// const removeBlanks = require('../../lib/remove_blank_fields')
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `req.user`
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
-const Groceries = require('./../models/groceries')
-
-const handle404 = require('./../../lib/custom_errors')
-
-const requireToken = passport.authenticate('bearer', { session: false })
 // index!!
-router.get('/groceries', (req, res, next) => {
-  // const id = req.params.id
-  Groceries.find()
-    .populate('user')
+router.get('/groceries', requireToken, (req, res, next) => {
+  Groceries.find({owner: req.user._id})
+    .populate('owner')
     .then(handle404)
     .then(groceries => res.json({ groceries }))
     .catch(next)
 })
-// .then(restaurants => {
-//   res.status(200).json({ restaurants })
-// })
-// .catch(next)
-// })
+
 router.get('/groceries/:id', requireToken, (req, res, next) => {
   const id = req.params.id
   Groceries.findById(id)
@@ -28,7 +43,6 @@ router.get('/groceries/:id', requireToken, (req, res, next) => {
     .catch(next)
 })
 router.post('/groceries', requireToken, (req, res, next) => {
-  console.log('It worked!', req.body)
   req.body.groceries.owner = req.user.id
   const groceriesData = req.body.groceries
   Groceries.create(groceriesData)
