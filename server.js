3904b2594d9b220e6508a455f52c7baf10bafa61
Maxwell Kwarteng <maxtheflash007@gmail.com>
2022-04-08 17:26:15 -0400

    Added all the modified files to Max's Trofí App

--- server.js
+++ server.js
@@ -39,19 +39,6 @@ const app = express()
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}` }))
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE')
-  res.header('Access-Control-Allow-Credentials', true)
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204)
-  }
-  next()
-})
 // define port for API to run on
 const port = process.env.PORT || serverDevPort
 
